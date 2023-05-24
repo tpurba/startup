2b612d6b132fc0b5356ee948d848c18df4c16b38
tpurba <tpurba@byu.edu>
2023-05-24 00:28:13 -0600

    jump physics works

---
+++ game.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () =>
+{
+  const dino = document.querySelector('.dino');
+  let bottom = 0;
+  let gravity = 0.9;
+  let isJumping = false;
+  function jump()
+  {
+    if(isJumping){
+      return;
+    }
+    let timerUpId = setInterval( function (){
+
+      if(bottom > 250){
+        clearInterval(timerUpId);
+        let timerDownId = setInterval( function(){
+          if(bottom < 0){
+            clearInterval(timerDownId);
+            isJumping = false;
+          }
+          bottom -=5;
+          bottom = bottom * gravity;
+          dino.style.bottom = bottom + 'px';
+      }, 20);
+      }
+      isJumping = true;
+      bottom += 30;
+      bottom = bottom * gravity;
+      dino.style.bottom = bottom + 'px';
+    }, 20);
+   
+  }
+
+  function control(e){
+    //space bar
+    if(e.keyCode === 32){
+      jump();
+    }
+  }
+  //if key is pressed it goes to control function 
+  document.addEventListener('keydown', control);
+})
