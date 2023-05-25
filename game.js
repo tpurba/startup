d0c4d5d4d0291e3afeb0468740bff5348f491110
tpurba <tpurba@byu.edu>
2023-05-25 13:46:27 -0600

    completed cactus and blocks

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const dino = document.querySelector('.dino');
-  const block = document.querySelector('.obstacle');
+  const cactus = document.querySelector('.obstacle');
+  const grid = document.querySelector('.grid');
   let bottom = 0;
   let gravity = 0.9;
   let isJumping = false;
@@ -11,10 +12,10 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     let timerUpId = setInterval(function () {
 
-      if (bottom > 250) {
+      if (bottom > 170) {
         clearInterval(timerUpId);
         let timerDownId = setInterval(function () {
-          if (bottom < 0) {
+          if (bottom < 50) {
             clearInterval(timerDownId);
             isJumping = false;
           }
@@ -39,5 +40,50 @@ document.addEventListener('DOMContentLoaded', () => {
   //if key is pressed it goes to control function 
   document.addEventListener('keydown', control);
 
-  
+  function generateSquare() {
+    let randomTime = Math.random() * 4000;
+    let squarePosition = 1000;
+    const square = document.createElement('div');
+    if(!isGameOver){
+      square.classList.add('boxbox');
+    }
+    grid.appendChild(square);
+    square.style.left = squarePosition + 'px';
+    let timerId = setInterval(function () {
+      if(squarePosition > 0 && squarePosition < 60 && bottom < 80){
+        clearInterval(timerId);
+        alert("Game Over");
+        isGameOver = true;
+        //remove all the children 
+        while(grid.firstChild){
+          grid.removeChild(grid.lastChild);
+        }
+      }
+      if(squarePosition < 0 && grid.contains(square)){
+        grid.removeChild(square);
+      }
+      squarePosition -= 10;
+      square.style.left = squarePosition + 'px';
+      
+    }, 20)
+    if(!isGameOver) {
+      setTimeout(generateSquare, randomTime);
+    }
+  }
+  generateSquare();
+
+  let isAlive = setInterval(function () {
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    //console.log(dinoTop);
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+    //console.log(cactusLeft);
+    //cactus exists between -22 and 27 and dinotop is above 600 or equal 
+    if ((cactusLeft < 27) && (cactusLeft > -22) && dinoTop >= 600) {
+      //alert("Game over");
+
+    }
+
+
+  }, 10);
+
 }); 
