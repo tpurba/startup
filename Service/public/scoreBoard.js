38391f24e2692237a2f5a29790abae1e7d75d3bc
tpurba <tpurba@byu.edu>
2023-06-01 19:05:29 -0600

    got get to work

--- Service/public/scoreBoard.js
+++ Service/public/scoreBoard.js
@@ -1,38 +1,48 @@
-function loadScores() {
-  console.log("In loadScores");
+async function loadScores() {
+  console.log("loadScores");
   let scores = [];
-  const scoresText = localStorage.getItem('scores');
-  if (scoresText) {
-    scores = JSON.parse(scoresText);
+  try {
+    // Get the latest high scores from the service
+    console.log("getting from server..");
+    const response = await fetch('/sb/scores');
+    scores = await response.json();
+
+    // Save the scores in case we go offline in the future
+    localStorage.setItem('scores', JSON.stringify(scores));
+  } catch {
+    console.log("could not get scores from the database loading local");
+    // If there was an error then just use the last saved scores
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+      scores = JSON.parse(scoresText);
+    }
   }
 
+  displayScores(scores);
+}
+
+function displayScores(scores) {
   const tableBodyEl = document.querySelector('#scores');
-  console.log("TableBody:", tableBodyEl);
 
   if (scores.length) {
-    console.log(scores.length);
+    // Update the DOM with the scores
     for (const [i, score] of scores.entries()) {
-      console.log(i);
       const positionTdEl = document.createElement('td');
       const nameTdEl = document.createElement('td');
       const scoreTdEl = document.createElement('td');
       const dateTdEl = document.createElement('td');
 
       positionTdEl.textContent = i + 1;
-      console.log("Pos: "+ positionTdEl.textContent);
       nameTdEl.textContent = score.name;
-      console.log("Name: "+ nameTdEl.textContent);
       scoreTdEl.textContent = score.score;
-      console.log("Score: "+ scoreTdEl.textContent);
       dateTdEl.textContent = score.date;
-      console.log("Date: "+ dateTdEl.textContent);
 
       const rowEl = document.createElement('tr');
       rowEl.appendChild(positionTdEl);
       rowEl.appendChild(nameTdEl);
       rowEl.appendChild(scoreTdEl);
       rowEl.appendChild(dateTdEl);
-      console.log("RowEl:" , rowEl);
+
       tableBodyEl.appendChild(rowEl);
     }
   } else {
