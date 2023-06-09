b944d115a2913dca4f7199dc67a26a195f8065ae
tpurba <tpurba@byu.edu>
2023-06-09 10:59:26 -0600

    changed client name

--- Service/database.js
+++ Service/database.js
@@ -5,7 +5,7 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('dino');
+const db = client.db('dinoRun');
 const userCollection = db.collection('user');
 const scoreCollection = db.collection('score');
 
