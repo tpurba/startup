047dad8f51c62e69f49cd14b24cefee5375660bb
tpurba <tpurba@byu.edu>
2023-06-19 15:25:54 -0600

    finished game

--- dinoGame/demoVite/src/game/dinoGame.jsx
+++ dinoGame/demoVite/src/game/dinoGame.jsx
@@ -84,12 +84,61 @@ export function DinoGame(props) {
         clearInterval();
         alert("Game Over! \n Score:" + score);
         //store the score and the players userName together for database 
-        // saveScore(score);
+        saveScore(score);
         score = 0;
-        // window.location.href = 'scoreBoard.html';
+        window.location.href = '/score';
       }
 
     }, 10);
+  }
+  async function saveScore(score) {
+    const date = new Date().toLocaleDateString();
+    const newScore = { name: userName, score: score, date: date };
+
+    try {
+      const response = await fetch('/api/score', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(newScore),
+      });
+
+      // Let other players know the game has concluded
+      GameNotifier.broadcastEvent(userName, GameEvent.End, newScore);
+
+      // Store what the service gave us as the high scores
+      const scores = await response.json();
+      localStorage.setItem('scores', JSON.stringify(scores));
+    } catch {
+      // If there was an error then just track scores locally
+      updateScoresLocal(newScore);
+    }
+  }
+
+  function updateScoresLocal(newScore) {
+    let scores = [];
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+      scores = JSON.parse(scoresText);
+    }
+
+    let found = false;
+    for (const [i, prevScore] of scores.entries()) {
+      if (newScore > prevScore.score) {
+        scores.splice(i, 0, newScore);
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      scores.push(newScore);
+    }
+
+    if (scores.length > 10) {
+      scores.length = 10;
+    }
+
+    localStorage.setItem('scores', JSON.stringify(scores));
   }
     return (
         <div className="world">
